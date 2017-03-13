1b488d0d4e996805cdacfaac44d079166329fa6c
Aniloid2 <brianformento@hotmail.co.uk>
2017-03-13 10:37:48 +0000

    aws giving prbles

--- QuadLink/static/js/Get_the_data.js
+++ QuadLink/static/js/Get_the_data.js
@@ -1,22 +1,5 @@
-// window.onload = initAll;
-//https://www.awwwards.com/creating-3d-cube-a-practical-guide-to-three-js-with-live-demo.html
-//https://wowmoron.wordpress.com/2013/10/21/webgl-and-javascript-drawing-simple-3d-shapes-using-three-js/
+
 function get_the_data() {
-	// var po = document.getElementById('po');
-	// console.log(po)
-
-	// var inside = document.getElementById('inside');
-	// console.log(inside)
-
-	// var up = function(element, val) {
-	// 	console.log('Whole object',val)
-	// 	element.textContent = val;
-	// }
-
-	// var insidefun = function(element, val) {
-	// 	console.log(val)
-	// 	element.textContent = val;
-	// }
 
 	var video = document.getElementById('video_stream');
 
@@ -55,24 +38,12 @@ function get_the_data() {
 
 	firebase.initializeApp(config);
 
-	// const preObject = document.getElementById('object');
-
 	const dbRefObject = firebase.database().ref().child(quad);
-
-	// dbRefObject.on('value', function(snapshot) {
-	// 	console.log(snapshot.val());
-	// 	up(po,snapshot.val());
-	// });
 
 
 	const axis = firebase.database().ref().child(quad).child("axis")
 
 	const pitch = firebase.database().ref().child(quad).child("axis").child("pitch")
-
-	// pitch.on('value', function(snapshot) {
-	// 	console.log(snapshot.val())
-	// 	insidefun(inside, snapshot.val())
-	// });
 
 	const roll = firebase.database().ref().child(quad).child("axis").child("roll")
 
